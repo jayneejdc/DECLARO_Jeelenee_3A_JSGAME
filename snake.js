47b69ee209233595b3328d72878ee2999b032827
Jaynee De Claro <jeeleneejaysondeclaro@gmail.com>
2017-10-15 22:35:44 -0700

    declared variables

---
+++ snake.js
@@ -0,0 +1,18 @@
+//settings
+var snakeX = 2;
+var snakeY = 2;
+var height = 30;
+var width = 30;
+var interval = 100;
+var incerement = 1;
+
+//game variables
+var tailX = [snakeX];
+var tailY = [snakeY];
+var fX;
+var fY;
+var running;
+var gameOver;
+var direction = -1; //up = 0, down = -1, left = 1, right = 2
+var int;
+
